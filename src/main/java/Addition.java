5a3d6cf695a529a4bfd399d1c9dd114ed9372dcd
Richard Fehling <78547018+Skysurfer65@users.noreply.github.com>
2021-05-19 15:01:25 +0200

    Update Addition.java

--- src/main/java/Addition.java
+++ src/main/java/Addition.java
@@ -4,7 +4,6 @@ import java.text.DecimalFormat;
 public class Addition {
 
     void addition(CalculatorMain calculator) {
-        //Testing channges
         //Menu addition
         calculator.menu.addition();
 
